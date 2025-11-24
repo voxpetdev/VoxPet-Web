044b74843c5b64fbd33c39e9229b0ea4b6a5a17b
mat0wa3 <mmateo1804@gmail.com>
2025-11-24 14:51:29 -0500

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+  adapter: vercel(),
+  output: 'server'
 
-  output: 'server',
-
-  adapter: node({
-    mode: 'standalone'
-  })
 });

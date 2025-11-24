e01e6eaec263789cc133e787e6fca2e75533e71c
mat0wa3 <mmateo1804@gmail.com>
2025-11-24 01:16:41 -0500

    feat: add @astrojs/node adapter for standalone server mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  output: 'server'
+
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
